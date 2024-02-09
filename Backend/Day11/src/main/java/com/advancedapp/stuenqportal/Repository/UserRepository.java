2a7c3392401949135f0269f69a6e6e82e31da37f
deepakkannan7 <114062968+deepakkannan7@users.noreply.github.com>
2024-02-09 12:30:32 +0530

    Add files via upload

---
+++ Backend/Day11/src/main/java/com/advancedapp/stuenqportal/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.advancedapp.stuenqportal.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.advancedapp.stuenqportal.Entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
