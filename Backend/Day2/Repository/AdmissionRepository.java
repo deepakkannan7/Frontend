f8b96dcd74cbb0b891f56030346528d46d87cf3e
deepakkannan7 <114062968+deepakkannan7@users.noreply.github.com>
2024-02-09 12:10:19 +0530

    Delete Backend/Day2 directory

--- Backend/Day2/Repository/AdmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.advancedapp.stuenqportal.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.advancedapp.stuenqportal.Entity.Admission;
-
-public interface AdmissionRepository extends JpaRepository<Admission, Long> {
-}
-
